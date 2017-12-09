6740eefd3868a7db0b003ae8f0ceb4d38df5c055
Vivek Shah <vivek.shah93@gmail.com>
2017-12-08 19:57:28 -0500

    update files

--- app.js
+++ app.js
@@ -2,30 +2,24 @@ const express = require("express");
 let app = express();
 const fs = require('fs');
 
-
-//let configRoutes = require("routes");
-
-//configRoutes(app);
-
-
 app.get("/:id", function (req, res) {
 let donutID = req.params.id;
-fs.readFile('./donut.json', "utf-8", (error, data) => {
+fs.readFile('./carData.json', "utf-8", (error, data) => {
         console.log(data);
         if (error) throw error;
         obj = JSON.parse(data);
-for  (i = 0; i < obj.length; i++)
- { 
-    if(obj[i].id==donutID)
-    {
-        res.json(obj[i]);
-    }
-}
-});
-});
+		for  (i = 0; i < obj.length; i++)
+ 		{ 
+    		if(obj[i].id==donutID)
+    		{
+        		res.json(obj[i]);
+    		}
+		}
+		});
+	});
 
 app.get("/", function (req, res) {
-    fs.readFile('./donut.json', "utf-8", (error, data) => {
+    fs.readFile('./carData.json', "utf-8", (error, data) => {
         console.log(data);
         if (error) throw error;
         obj = JSON.parse(data);
@@ -36,5 +30,5 @@ app.get("/", function (req, res) {
 
 app.listen(8080, () => {
     console.log("We've now got a server!");
-    console.log("Your routes will be running on http://localhost:8080");
+    console.log("Routes are running on http://localhost:8080");
 });
